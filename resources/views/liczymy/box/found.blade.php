586062a77c0419a6e1a3d0854d5f361d22b8639d
Marcin Lawniczak <marcin.safmb@wp.pl>
2018-01-12 00:42:38 +0100

    Silent alarm

--- resources/views/liczymy/box/found.blade.php
+++ resources/views/liczymy/box/found.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
+@section('styles')
+    <style>
+        {{-- TODO wycentrować ten jebany przycisk i checkbox --}}
+        .center-block {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+@endsection
+
 @section('content')
     <h4>Znaleziono puszkę {{ $box->id }}</h4>
     <table class="table table-striped table-hover">
@@ -21,30 +32,33 @@
     <ul style="text-align: center; font-size: 2em;list-style-type: none;">
         <li>Potwierdź że dane z puszki i identyfikatora są zgodne z wyświetlonymi.</li>
         <li>Potwierdź że puszka nie nosi śladów uszkodzeń.</li>
-        <li>Nie wydawaj rozliczonej puszki wolontariuszowi.</li>
+        <li>Nie oddawaj rozliczonej puszki wolontariuszowi.</li>
     </ul>
-    <form class="form-horizontal" method="POST" action="{{ route('box.findConfirm') }}">
-        <fieldset>
+    <div class="center-block">
+        <form class="form-horizontal" method="POST" action="{{ route('box.findConfirm') }}">
+            <fieldset>
 
-        {{ csrf_field() }}
+                {{ csrf_field() }}
 
-        {{-- Cichy alarm --}}
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="silent-alarm">PU</label>
-            <input type="checkbox" id="silentalarm" name="silentalarm" @if(old('silentalarm')) checked @endif>
-        </div>
-        {{-- Przekazujemy ID puszki --}}
-        <input type="hidden" value="{{ $box->id }}" name="boxID">
-        <!-- Button -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="singlebutton">Zgodność z danymi rzeczywistymi</label>
-            <div class="col-md-4">
-                <button id="singlebutton" name="singlebutton" class="btn btn-success">Potwierdzam</button>
-            </div>
-        </div>
+                {{-- Cichy alarm --}}
+                <div class="form-group center-block">
+                    <label class="control-label" for="silent-alarm">PU</label>
+                    <input type="checkbox" id="silentalarm" name="silentalarm" @if(old('silentalarm')) checked @endif>
+                </div>
+                {{-- Przekazujemy ID puszki --}}
+                <input type="hidden" value="{{ $box->id }}" name="boxID">
+                <!-- Button -->
+                <div class="form-group center-block">
+                    <label class="control-label" for="singlebutton">Zgodność z danymi rzeczywistymi</label><br>
+                    <div class="">
+                        <button id="singlebutton" name="singlebutton" class="btn btn-success btn-lg">Potwierdzam</button>
+                    </div>
+                </div>
 
 
-        </fieldset>
-    </form>
+            </fieldset>
+        </form>
+    </div>
+
 
 @endsection
