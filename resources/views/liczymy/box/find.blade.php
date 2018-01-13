6fb3eaa0f910b3e12b07e4997b7eae7a267fb29d
Marcin Lawniczak <marcin.safmb@wp.pl>
2018-01-13 05:03:50 +0100

    fix

--- resources/views/liczymy/box/find.blade.php
+++ resources/views/liczymy/box/find.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="form-horizontal" method="POST" action="{{ route('box.find.post') }}" autocomplete="off">
+    <form class="form-horizontal" method="POST" action="{{ route('box.find.post') }}" autocomplete="off" role="presentation">
         <fieldset>
 
         {{ csrf_field() }}
@@ -9,11 +9,13 @@
         <!-- Form Name -->
         <legend>Znajdź puszkę do rozliczenia</legend>
 
+        {{-- Hidden input to trick chrome --}}
+        <input style="display:none">
         <!-- Text input-->
         <div class="form-group">
             <label class="col-md-4 control-label" for="collectorIdentifier">Numer identyfikatora</label>
             <div class="col-md-4">
-                <input id="collectorIdentifier" name="collectorIdentifier" type="text" placeholder="Np. 235" class="form-control input-md" required="" autocomplete="off">
+                <input readonly onfocus="this.removeAttribute('readonly');"  id="collectorIdentifier" name="collectorIdentifier" type="text" placeholder="Np. 235" class="form-control input-md" required="" autocomplete="new-password">
                 <span class="help-block">Z puszki (przed ukośnikiem)</span>
             </div>
         </div>
