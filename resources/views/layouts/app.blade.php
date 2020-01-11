e6966fb1f747e690dda570f2539ce72c32b5cf61
Marcin Ławniczak <marcin@lawniczak.me>
2020-01-11 15:19:12 +0100

    Menu for collectorcoordinator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
                             <li><a href="{{ route('login') }}">Logowanie</a></li>
                         @else
 
-                            @if(Auth::user()->hasAnyRole(['admin', 'superadmin']))
+                            @if(Auth::user()->hasAnyRole(['collectorcoordinator', 'admin', 'superadmin']))
                             <li>
                                 <a href="{{ route('box.verify.list') }}">
                                     <b>Przeliczone puszki</b>
@@ -107,7 +107,7 @@
                             </li>
                             @endif
                             {{-- Puszki --}}
-                                @if(Auth::user()->hasAnyRole(['admin', 'superadmin']))
+                                @if(Auth::user()->hasAnyRole(['collectorcoordinator', 'admin', 'superadmin']))
 
                                 <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
