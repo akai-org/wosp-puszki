fc3a7c9e88cd76c79da29d53aa7ffc479d7c7cad
Marcin Lawniczak <marcin@lawniczak.me>
2021-01-15 18:29:55 +0100

    Add WS conf change

--- resources/js/pusher_setup.js
+++ resources/js/pusher_setup.js
@@ -8,6 +8,7 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: false,
+    forceTLS: true,
+    encrypted: false,
     enabledTransports: ['ws', 'wss'],
 });
