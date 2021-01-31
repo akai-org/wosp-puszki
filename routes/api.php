cff8520eae4aa3fdff267dfef137c9356d4e8ac5
Marcin Ławniczak <marcin@lawniczak.me>
2021-01-31 09:32:18 +0100

    Add storage

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,35 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/storex', function (Request $request){
+    try {
+        Storage::disk('local')->put('raw.txt', $request->input('total'));
+        return Storage::get('raw.txt');
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+});
+
+Route::post('/store/json', function (Request $request){
+    try {
+        Storage::disk('local')->put('raw2.txt', $request->getContent());
+        return Storage::get('raw2.txt');
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+});
+
+Route::get('/json', function () {
+    return Storage::get('raw2.txt');
+});
+
+
+Route::get('/get', function () {
+    return 'x';
+});
+
+Route::get('/raw', function () {
+    header('Refresh: 3;');
+    return Storage::get('raw.txt');
+});
