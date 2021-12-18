ca419deba58d113f94503d7602215fe19bc972e8
Kacper Nadstoga <kapix007@gmail.com>
2021-12-18 21:09:29 +0100

    write styles to Found.vue

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,10 +37,12 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request)
     {
-        return array_merge(parent::share($request), [
+        return array_merge(parent::share($request), array([
             "auth.user" => function () use ($request) {
-                return User::with('roles')->find(\Auth::user()->id);
+                    if(\Auth::user()){
+                        return User::with('roles')->findOrFail(\Auth::user()->id);
+                    }
             },
-        ]);
+        ]));
     }
 }
--- resources/js/Pages/Box/Found.vue
+++ resources/js/Pages/Box/Found.vue
@@ -1,49 +1,57 @@
 <template>
 
     <Head title="Welcome" />
+    <div class="flex flex-col md:flex items-center justify-around  py-2 px-8 md:px-12">
+        <h4 class="text-3xl font-bold mx-4 my-4 text-gray-800 md:text-4xl">Znaleziono puszkę <span class="text-red-700">{{ box.id }}</span></h4>
+        <table class="table-fixed w-full text-sm rounded-lg">
+            <tbody>
+            <tr class="bg-gray-300 rounded-lg">
+                <td class="p-6 text-base font-bold tracking-wide text-left">Wolontariusz</td>
+                <td class="p-6 text-base text-gray-800">{{ collector.firstName }} {{ collector.lastName }}</td>
+            </tr>
+            <tr class="bg-white rounded-lg">
+                <td class="p-6 text-base font-bold tracking-wide text-left">Numer identyfikatora i na puszce</td>
+                <td class="p-6 text-base text-gray-800">{{ collector.identifier }}</td>
+            </tr >
 
-    <h4>Znaleziono puszkę {{ box.id }}</h4>
-    <table class="table table-striped table-hover">
-        <tbody>
-        <tr>
-            <td>Wolontariusz</td>
-            <td>{{ collector.firstName }} {{ collector.lastName }}</td>
-        </tr>
-        <tr>
-            <td>Numer identyfikatora i na puszce</td>
-            <td>{{ collector.identifier }}</td>
-        </tr>
-        @if(Auth::user()->hasAnyRole(['admin', 'superadmin']))
-        {{$page.props.auth.user}}
-        <tr>
-            <td>ID puszki w bazie</td>
-            <td>{{ box.id }}</td>
-            @endif
-        </tr>
-        </tbody>
-    </table>
-    <ul style="text-align: center; font-size: 2em;list-style-type: none;">
-        <li>Potwierdź, że dane z puszki i identyfikatora są zgodne z wyświetlonymi.</li>
-        <li>Potwierdź, że puszka nie nosi śladów uszkodzeń.</li>
-        <li>Nie oddawaj rozliczonej puszki wolontariuszowi.</li>
-    </ul>
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4 text-center">
-            <form @submit.prevent="submit" class="form-horizontal">
-                <fieldset>
-                    <!-- Button -->
-                    <div class="form-group">
-                        <label class="control-label" for="singlebutton">Zgodność z danymi rzeczywistymi</label><br>
-                        <div class="">
-                            <jet-button class="ml-4" id="singlebutton" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <tr v-if="flag"  class="bg-gray-300 rounded-lg">
+                <td class="p-6 text-base font-bold tracking-wide text-left ">ID puszki w bazie</td>
+                <td class="p-6 text-base text-gray-800 ">{{ box.id }}</td>
+            </tr>
+            </tbody>
+        </table>
+
+
+        <div class="bg-white mt-4 rounded-lg w-full shadow-md lg:w-1/3 ">
+            <ul class="divide-y divide-gray-100">
+                <li class="p-3 hover:text-red-700" >
+                    Potwierdź, że dane z puszki i identyfikatora są zgodne z wyświetlonymi.
+                </li>
+                <li class="p-3 hover:text-red-700">
+                    Potwierdź, że puszka nie nosi śladów uszkodzeń.
+                </li>
+                <li class="p-3 hover:text-red-700">
+                    Nie oddawaj rozliczonej puszki wolontariuszowi.
+                </li>
+            </ul>
+        </div>
+
+        <form @submit.prevent="submit" class="bg-white shadow-md rounded px-8 pt-6 pb-8 my-4">
+                    <fieldset>
+                        <!-- Button -->
+                        <div class="mt-4 mb-4 flex flex-col content-center">
+                            <jet-label class="block text-gray-700 text-xl underline underline-offset-8 font-bold mb-2"  for="singlebutton">Zgodność z danymi rzeczywistymi</jet-label>
+                            <br>
+                            <jet-button class="text-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                                 Potwierdzam
                             </jet-button>
                         </div>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
+                    </fieldset>
+                </form>
     </div>
+
+
+
 
 </template>
 
@@ -51,26 +59,31 @@
 
 import { Head } from '@inertiajs/inertia-vue3';
 import JetButton from '@/Jetstream/Button.vue';
+import JetLabel from '@/Jetstream/Label.vue';
 
 export default {
     name: 'Box/Found',
     components: {
         Head,
-        JetButton
-
+        JetButton,
+        JetLabel,
     },
     props: {
         box: Object,
-        collector: Object
+        collector: Object,
+        flag: Boolean
     },
     mounted() {
-        console.log(this.$page.props);
+        // console.log(this.$page.props);
+        this.flag = this.$page.props.auth.user ? true : false;
+
     },
     data() {
         return {
             form: this.$inertia.form({
                 boxID: this.$props.box.id
-            })
+            }),
+
         }
     },
 
