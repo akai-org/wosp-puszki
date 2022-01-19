3ac8c8cefc0ec8da911a4b2e39de491710d41e5e
Marcin Lawniczak <marcin@lawniczak.me>
2022-01-19 18:04:03 +0100

    Add containers, reformat html

--- resources/views/liczymy/box/create.blade.php
+++ resources/views/liczymy/box/create.blade.php
@@ -1,33 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="form-horizontal" method="POST" action="{{ route('box.create.post') }}" autocomplete="off" onsubmit="singlebutton.disabled = true; return true;">
-        <fieldset>
+    <div class="container">
+        <form class="form-horizontal" method="POST" action="{{ route('box.create.post') }}" autocomplete="off"
+              onsubmit="singlebutton.disabled = true; return true;">
+            <fieldset>
 
             {{ csrf_field() }}
 
             <!-- Form Name -->
-            <legend>Wydawanie puszki</legend>
+                <legend>Wydawanie puszki</legend>
 
-            <!-- Text input-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="collectorIdentifier">Numer identyfikatora</label>
-                <div class="col-md-4">
-                    {{-- https://developer.mozilla.org/en-US/docs/Web/Security/Securing_your_site/Turning_off_form_autocompletion --}}
-                    <input id="collectorIdentifier" name="collectorIdentifier" type="text" placeholder="Np. 345" class="form-control input-md" required="" value="{{  old('collectorIdentifier') }}" autocomplete="nope">
-                    <span class="help-block">Z identyfikatora (Na puszce <b>przed ukośnikiem</b>)</span>
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="collectorIdentifier">Numer identyfikatora</label>
+                    <div class="col-md-4">
+                        {{-- https://developer.mozilla.org/en-US/docs/Web/Security/Securing_your_site/Turning_off_form_autocompletion --}}
+                        <input id="collectorIdentifier" name="collectorIdentifier" type="text" placeholder="Np. 345"
+                               class="form-control input-md" required="" value="{{  old('collectorIdentifier') }}"
+                               autocomplete="nope">
+                        <span class="help-block">Z identyfikatora (Na puszce <b>przed ukośnikiem</b>)</span>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Button -->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="singlebutton">Dodaj puszkę</label>
-                <div class="col-md-4">
-                    <button id="singlebutton" name="singlebutton" class="btn btn-success">Dodaj puszkę</button>
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="singlebutton">Dodaj puszkę</label>
+                    <div class="col-md-4">
+                        <button id="singlebutton" name="singlebutton" class="btn btn-success">Dodaj puszkę</button>
+                    </div>
                 </div>
-            </div>
 
-        </fieldset>
-    </form>
+            </fieldset>
+        </form>
+    </div>
 
 @endsection
--- resources/views/liczymy/collector/list.blade.php
+++ resources/views/liczymy/collector/list.blade.php
@@ -1,41 +1,41 @@
 @extends('layouts.app')
 
 @section('styles')
-
-<script type="text/javascript" src="{{ asset('js/sortable.js') }}"></script>
-
+    <script type="text/javascript" src="{{ asset('js/sortable.js') }}"></script>
 @endsection
 
 @section('content')
 
-    @if(auth()->user()->hasAnyRole(['collectorcoordinator', 'superadmin']))
-    <div class="text-right" style="margin-right: 15px;">
-        <a class="btn btn-success btn-lg" href="{{ route('collector.create') }}">
-            Dodaj wolontariusza
-        </a>
-    </div>
-    @endif
+    <div class="container">
+        @if(auth()->user()->hasAnyRole(['collectorcoordinator', 'superadmin']))
+            <div class="text-right" style="margin-right: 15px;">
+                <a class="btn btn-success btn-lg" href="{{ route('collector.create') }}">
+                    Dodaj wolontariusza
+                </a>
+            </div>
+        @endif
 
-    <table id="volounteers" class="sortable table table-striped table-hover">
-        <thead>
-        <tr>
-            <th data-sort="int">Numer ID</th>
-            <th data-sort="string">Imię</th>
-            <th data-sort="string">Nazwisko</th>
-            <th data-sort="float">Kwota zebrana (tylko PLN)</th>
-            <th data-sort="string">Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($collectors as $collector)
-        <tr>
-            <td>{{ $collector->identifier }}</td>
-            <td>{{ $collector->firstName }}</td>
-            <td>{{ $collector->lastName }}</td>
-            <td>{{ $collector->boxes()->sum('amount_PLN') }}</td>
-            <td style="background-color: {{$status[$collector->identifier]['color'] }};font-weight: bold;">{{ $status[$collector->identifier]['message'] }}</td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
+        <table id="volounteers" class="sortable table table-striped table-hover">
+            <thead>
+            <tr>
+                <th data-sort="int">Numer ID</th>
+                <th data-sort="string">Imię</th>
+                <th data-sort="string">Nazwisko</th>
+                <th data-sort="float">Kwota zebrana (tylko PLN)</th>
+                <th data-sort="string">Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($collectors as $collector)
+                <tr>
+                    <td>{{ $collector->identifier }}</td>
+                    <td>{{ $collector->firstName }}</td>
+                    <td>{{ $collector->lastName }}</td>
+                    <td>{{ $collector->boxes()->sum('amount_PLN') }}</td>
+                    <td style="background-color: {{$status[$collector->identifier]['color'] }};font-weight: bold;">{{ $status[$collector->identifier]['message'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
