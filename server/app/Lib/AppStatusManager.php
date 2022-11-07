c0bad2350daaa556a2f97cd0953c31c0c1ddf11b
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-11-07 18:50:26 +0100

    Menedżer dla statusów (par klucz-wartość)

---
+++ server/app/Lib/AppStatusManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Lib;
+
+use App\AppStatus;
+
+/**
+ * A utility used to store and retrieve key-value pairs
+ * to and from the database
+ */
+class AppStatusManager
+{
+    /**
+     * The current amount collected in the money box
+     *
+     * @var string
+     */
+    public const MONEYBOX_VALUE = 'moneybox.value';
+
+    /**
+     * Reads a value stored with the given key in the database
+     */
+    public static function readStatusValue(string $status_key, string $defaultValue): string {
+        $status = AppStatus::find($status_key);
+
+        if($status === null) return $defaultValue;
+        return $status->value;
+    }
+
+    /**
+     * Saves the value to the database with the specified key
+     */
+    public static function saveStatusValue(string $status_key, string $value): void {
+        AppStatus::updateOrCreate(
+            [ 'id' => $status_key ],
+            [ 'value' => $value ]
+        );
+    }
+}
