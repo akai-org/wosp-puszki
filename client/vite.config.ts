7b79322af481fd3ce68ab909a7f3e83884dcd299
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2022-11-11 13:06:40 +0100

    add theme to config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { getThemeVariables } from 'antd/dist/theme';
+import vitePluginImp from 'vite-plugin-imp';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'btn-primary-bg': '#CF1322',
+          //fonts
+          'font-size-base': '16px',
+          'font-size-lg': '24px',
+          'heading-1-size': '40px',
+          'heading-2-size': '32px',
+        },
+
+        javascriptEnabled: true,
+      },
+    },
+  },
 });
