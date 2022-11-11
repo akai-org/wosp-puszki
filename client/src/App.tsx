c65bf009cfe30f46334bf173f325bfe5319272c8
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2022-11-11 13:11:45 +0100

    count button with theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,7 +21,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
