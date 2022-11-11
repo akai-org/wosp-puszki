b93d354cc12aa13ef4fc0fdcfdf2e8793bad7b05
Now4czyk <kacpern30@gmail.com>
2022-11-11 18:53:19 +0100

    fixed remarks and added barrels

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,5 @@
-import { Space } from 'antd';
-import { LoginForm } from './components/forms/LoginForm/LoginForm';
-
 function App() {
-  return (
-    <Space
-      align="center"
-      style={{
-        width: '100vw',
-        height: '100vh',
-        justifyContent: 'center',
-      }}
-    >
-      <LoginForm />
-    </Space>
-  );
+  return <div>wosp-puszki</div>;
 }
 
 export default App;
