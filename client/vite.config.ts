8a441d0272d4204ae3b6d6fe2ec7c14bd1fb36b1
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2022-11-12 18:58:05 +0100

    delete unnecessary import

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { getThemeVariables } from 'antd/dist/theme';
 import vitePluginImp from 'vite-plugin-imp';
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
