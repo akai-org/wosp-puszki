b1f0f4d38759a7c6ee43269357a1657a54956ecb
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2022-12-02 22:34:55 +0100

    clearing unnecessary notes

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,5 @@
 import { Main } from '../../components/main';
 
-// needed:
-// separate component with all
-// inside that separated component for collected cause it will have api requests
 const MainPage: React.FC = () => <Main />;
 
 export default MainPage;
