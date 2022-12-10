3837340f5040ab53e8dbadb28e4723695637add1
Damian S <damian.sroczynski.22@gmail.com>
2022-12-10 21:29:03 +0100

    CreateTable component and two pages
    The CreateTable component was created to facilitate the process of creating pages that need a table.

--- client/src/pages/countedBoxes/BoxesApprovedPage/BoxesForApprovalPage.tsx
+++ client/src/pages/countedBoxes/BoxesApprovedPage/BoxesForApprovalPage.tsx
@@ -1,3 +1,92 @@
+import { CreateTable } from '@components/table/CreateTable';
+import { Typography, Space, Layout } from 'antd';
+import { useState, useEffect } from 'react';
+
+import type { TableColumns, DataType } from '@/utils';
+
+import s from './BoxesApprovedPage.module.css';
+
+const { Title } = Typography;
+const { Content } = Layout;
+
 export const BoxesForApprovalPage = () => {
-  return <p>BoxesForApprovalPage</p>;
+  const [data] = useState<DataType[]>([
+    {
+      key: '1',
+      name: 'C John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      date: '2021-01-01',
+    },
+    {
+      key: '2',
+      name: 'A Jim Green',
+      age: 42,
+      address: 'London No. 2 Lake Park',
+      date: '2022-01-01',
+    },
+    {
+      key: '3',
+      name: 'B Jim Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      date: '2012-01-01',
+    },
+  ]);
+
+  const columnsOptions: TableColumns[] = [
+    {
+      titleName: 'Name',
+      keyName: 'name',
+      sortType: 'string',
+      search: true,
+    },
+    {
+      titleName: 'Age',
+      keyName: 'age',
+      sortType: 'number',
+      search: false,
+    },
+    {
+      titleName: 'Address',
+      keyName: 'address',
+      sortType: 'string',
+      search: true,
+    },
+    {
+      titleName: 'Date',
+      keyName: 'date',
+      sortType: 'date',
+      search: true,
+    },
+    {
+      titleName: 'Actions',
+      keyName: 'actions',
+      actions: [
+        {
+          title: 'Approve',
+          link: '/countedBoxes/boxesApproved/approve/',
+        },
+        {
+          title: 'Show',
+          link: '/countedBoxes/boxesApproved/show/',
+        },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    //fetching data here
+  }, []);
+
+  return (
+    <Layout>
+      <Content className={s.content}>
+        <Space direction="vertical" size="small" className={s.space}>
+          <Title level={2}>Do zatwierdzenia</Title>
+          <CreateTable data={data} columnsOptions={columnsOptions} />
+        </Space>
+      </Content>
+    </Layout>
+  );
 };
--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -13,3 +13,25 @@ export interface Option {
 export interface IdNumber {
   idNumber: string;
 }
+
+interface actions {
+  title: string;
+  link: string;
+}
+
+export interface TableColumns {
+  titleName: string;
+  keyName: string;
+  sortType?: 'number' | 'string' | 'date';
+  search?: boolean;
+  actions?: actions[];
+}
+
+// gdy będą jakieś dane z backendu to podmienić na odpowiedni typ
+export interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  date: string;
+}
