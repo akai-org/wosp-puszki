585b2bcb904bbb1e3c817ce4b9e9ab548fd7d436
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2023-01-07 22:46:26 +0100

    modified css

--- client/src/components/forms/FindBoxForm/FindBoxForm.tsx
+++ client/src/components/forms/FindBoxForm/FindBoxForm.tsx
@@ -22,27 +22,38 @@ export const FindBoxForm = () => {
       <Button type="primary" className={s.break}>
         Nie chcę rozliczać dalej - przerwa
       </Button>
-      <FormWrapper onFinish={onSubmit} name="boxToSettleForm" className={s.form}>
-        <Title level={4}>Znajdź puszkę do rozliczenia</Title>
-        <Space className={s.inputContainer} size={0}>
-          <Text className={s.text}>Numer Identyfikatora:</Text>
-          <FormInput
-            name="idNumber"
-            className={s.input}
-            placeholder="Np. 123"
-            rules={[{ required: true, message: ID_NUMBER_REQUIRED }]}
-          />
+      <FormWrapper
+        onFinish={onSubmit}
+        name="boxToSettleForm"
+        className={s.form}
+        borderColor="black"
+      >
+        <Space direction="vertical">
+          <Title level={4} className={s.title}>
+            Znajdź puszkę do rozliczenia
+          </Title>
+          <Space direction="vertical" className={s.form}>
+            <Space className={s.inputContainer} size={0}>
+              <Text className={s.text}>Numer Identyfikatora:</Text>
+              <FormInput
+                name="idNumber"
+                className={s.input}
+                placeholder="Np. 123"
+                rules={[{ required: true, message: ID_NUMBER_REQUIRED }]}
+              />
+            </Space>
+            <FormSelect
+              name="boxType"
+              options={options}
+              placeholder="Wybierz rodzaj"
+              className={s.select}
+              rules={[{ required: true, message: TYPE_OF_BOX_REQUIRED }]}
+            />
+            <FormButton htmlType="submit" type="primary" className={s.submitButton}>
+              Wyszukaj puszkę
+            </FormButton>
+          </Space>
         </Space>
-        <FormSelect
-          name="boxType"
-          options={options}
-          placeholder="Wybierz rodzaj"
-          className={s.select}
-          rules={[{ required: true, message: TYPE_OF_BOX_REQUIRED }]}
-        />
-        <FormButton htmlType="submit" type="primary" className={s.submitButton}>
-          Wyszukaj puszkę
-        </FormButton>
       </FormWrapper>
     </Content>
   );
--- client/src/components/forms/FormWrapper/FormWrapper.tsx
+++ client/src/components/forms/FormWrapper/FormWrapper.tsx
@@ -5,14 +5,20 @@ import type { FC, ReactNode } from 'react';
 
 interface Props extends FormProps {
   label?: ReactNode;
+  borderColor?: ReactNode;
   children: ReactNode;
 }
 
-export const FormWrapper: FC<Props> = ({ label, children, ...rest }) => {
+export const FormWrapper: FC<Props> = ({
+  borderColor = 'red',
+  label,
+  children,
+  ...rest
+}) => {
   return (
     <Space direction="vertical" align="center">
       <Typography.Text className={s.title}>{label}</Typography.Text>
-      <Card size="small" className={s.card}>
+      <Card size="small" className={(borderColor = 'black' ? s.blackBorderCard : s.card)}>
         <Form className={s.form} {...rest}>
           {children}
         </Form>
