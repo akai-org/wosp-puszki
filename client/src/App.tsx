3036842c6c20d1966b70aa99f9ceddb4c116f1c9
Patryk Marczak <m.patryk@outlook.com>
2023-01-07 21:50:08 +0100

    Basic api testing request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,28 @@ import { routes } from './routes';
 import { Spin } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const router = createBrowserRouter(routes);
 
 export const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    fetch('https://wosp.ang3r.pl/sanctum/csrf-cookie', { credentials: 'include' })
+      .then((data) => {
+        console.log(data.headers);
+      })
+      .catch((e) => console.log(e));
+
+      // axios.get('https://wosp.ang3r.pl/sanctum/csrf-cookie', { withCredentials: true})
+      //     .then((data) => {
+      //         console.log(data.headers);
+      //     })
+      //     .catch((e) => console.log(e));
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} fallbackElement={<Spin />} />;
