c5f4a49920305156c794f0d6757e3c0e128a83e2
Patryk Marczak <m.patryk@outlook.com>
2023-01-24 20:55:36 +0100

    sensing request with attached auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,26 @@ import { Spin } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@assets/fonts/Oswald-Bold.ttf';
+import { useEffect } from 'react';
 
 const router = createBrowserRouter(routes);
 
 export const queryClient = new QueryClient();
 
 function App() {
+  //383
+  useEffect(() => {
+    // próbowałem też z decodeURI i decodeURIComponent - żadnej różnicy nie ma
+    const encoded = window.btoa('superadmin:zaq12wsx');
+    console.log(encoded);
+
+    fetch('http://localhost:8000/api/collectors/383/boxes/latestUncounted', {
+      headers: { Authorization: `Basic ${encoded}` },
+    })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} fallbackElement={<Spin />} />
