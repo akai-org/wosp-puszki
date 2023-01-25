5adb5cd8f12a4ab8847c416f3cc06decf7c3915d
Patryk Marczak <m.patryk@outlook.com>
2023-01-25 11:27:46 +0100

    Added useAuthContextValues hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,8 @@ import { Spin } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@assets/fonts/Oswald-Bold.ttf';
-import { createContext, useState } from 'react';
-import {
-  APIManager,
-  fetcher,
-  IAuthContext,
-  STORAGE_CREDENTIALS,
-  STORAGE_USERNAME,
-} from '@/utils';
+import { createContext } from 'react';
+import { IAuthContext, useAuthContextValues } from '@/utils';
 
 const router = createBrowserRouter(routes);
 
@@ -20,30 +14,8 @@ export const queryClient = new QueryClient();
 export const AuthContext = createContext<IAuthContext | null>(null);
 
 function App() {
-  const [credentials, updateCredentials] = useState<string | null>(() =>
-    localStorage.getItem(STORAGE_CREDENTIALS),
-  );
-  const [username, updateUsername] = useState<string | null>(() =>
-    localStorage.getItem(STORAGE_USERNAME),
-  );
-
-  const createCredentials = async (username: string, password: string) => {
-    const encodedCredentials = window.btoa(decodeURI(`${username}:${password}`));
-    await fetcher(APIManager.validateUserURL, {
-      headers: { Authorization: `Basic ${encodedCredentials}` },
-    });
-    localStorage.setItem(STORAGE_CREDENTIALS, encodedCredentials);
-    localStorage.setItem(STORAGE_USERNAME, username);
-    updateCredentials(encodedCredentials);
-    updateUsername(username);
-  };
-
-  const deleteCredentials = () => {
-    localStorage.removeItem(STORAGE_CREDENTIALS);
-    localStorage.removeItem(STORAGE_USERNAME);
-    updateCredentials(null);
-    updateUsername(null);
-  };
+  const { deleteCredentials, credentials, createCredentials, username } =
+    useAuthContextValues();
 
   return (
     <AuthContext.Provider
