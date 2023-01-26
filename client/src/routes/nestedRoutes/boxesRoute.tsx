ee71514be9a87f1d549bd67f81530744b845e4b9
Patryk Marczak <m.patryk@outlook.com>
2023-01-26 18:30:07 +0100

    Fixed absolute urls error

--- client/src/routes/nestedRoutes/boxesRoute.tsx
+++ client/src/routes/nestedRoutes/boxesRoute.tsx
@@ -9,11 +9,10 @@ import {
 import { InnerLayout } from '@/components';
 import { ProtectedRoute } from '@components/ProtectedRoute/ProtectedRoute';
 
-const giveBoxPagePath = '/boxes/give';
-const settleBoxPagePath = '/boxes/settle';
-const settleBoxPagePathCheckout = '/boxes/settle/4';
-const allBoxesPagePath = '/boxes';
-const unsettledBoxesPagePath = '/boxes/unsettled';
+const giveBoxPagePath = 'give';
+const settleBoxPagePath = 'settle';
+const settleBoxPagePathCheckout = 'settle/4';
+const unsettledBoxesPagePath = 'unsettled';
 
 const boxesSubroutes: RouteObject[] = [
   {
