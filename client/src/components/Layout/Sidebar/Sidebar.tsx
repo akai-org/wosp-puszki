8975e1eb1980b2b115d7b8e9759e1df9a677aca4
Patryk Marczak <m.patryk@outlook.com>
2023-01-28 23:47:01 +0100

    Subroutes fix

--- client/src/components/Layout/Sidebar/Sidebar.tsx
+++ client/src/components/Layout/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ const links = [
   { label: 'Strona Główna', url: '' },
   // { label: 'Przeliczone puszki', url: 'countedBoxes' },
   // { label: 'Administracja', url: 'admin' },
-  { label: 'Puszki', url: 'boxes/give' },
+  { label: 'Puszki', url: 'boxes' },
 ];
 
 export const Sidebar: FC = () => {
--- client/src/routes/nestedRoutes/boxesRoute.tsx
+++ client/src/routes/nestedRoutes/boxesRoute.tsx
@@ -24,7 +24,7 @@ const unsettledBoxesPagePath = 'unsettled';
 
 const boxesSubroutes: RouteObject[] = [
   {
-    path: giveBoxPagePath,
+    index: true,
     element: <GiveBoxPage />,
   },
   {
@@ -44,7 +44,7 @@ const boxesSubroutes: RouteObject[] = [
     element: <SettleBoxPageCheckout />,
   },
   {
-    index: true,
+    path: 'listBoxes',
     element: <ListBoxesPage />,
   },
   {
