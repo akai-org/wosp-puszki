c510129d0d442c922525af899efd145e4a9c27cf
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2023-01-29 10:35:59 +0100

    Fix create box message

--- server/app/Http/Controllers/CharityBoxController.php
+++ server/app/Http/Controllers/CharityBoxController.php
@@ -39,7 +39,7 @@ class CharityBoxController extends Controller
                 ->with('error', $e->getMessage());
         }
 
-        return view('liczymy.box.create')->with('success', 'Wydano puszkę wolontariuszowi ' . $box->collector->display . $box->display_id);
+        return view('liczymy.box.create')->with('message', 'Wydano puszkę wolontariuszowi ' . $box->collector->display . $box->display_id);
     }
 
     //Znajdź puszkę (formularz)
