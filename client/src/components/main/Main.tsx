034f96448d284e37e162ef5e8ef342a8bbcec60d
Patryk Marczak <m.patryk@outlook.com>
2023-01-29 10:17:14 +0100

    Works like a charm

--- client/src/components/main/Main.tsx
+++ client/src/components/main/Main.tsx
@@ -2,22 +2,25 @@ import { Layout, Button, Space } from 'antd';
 import { Collected } from './Collected';
 import s from './Main.module.css';
 import { useNavigate } from 'react-router-dom';
-import { useAmountsQuery } from '@/utils';
+import { useAmountsQuery, useAuthContext } from '@/utils';
 
 const { Content } = Layout;
 
 export const Main = () => {
   const navigate = useNavigate();
   const { data: amountsData } = useAmountsQuery();
+  const { username } = useAuthContext();
 
   // TODO: Change buttons to links
   return (
     <Content className={s.container}>
       <Collected pln={amountsData.amount_PLN} total={amountsData.amount_total_in_PLN} />
       <Space size={30}>
-        <Button onClick={() => navigate('boxes')} type="primary" className={s.buttons}>
-          Wydaj puszkę wolontariuszowi
-        </Button>
+        {username && parseInt(username?.slice(-2)) ? null : (
+          <Button onClick={() => navigate('boxes')} type="primary" className={s.buttons}>
+            Wydaj puszkę wolontariuszowi
+          </Button>
+        )}
         <Button onClick={() => navigate('boxes/settle')} className={s.buttons}>
           Rozlicz puszkę
         </Button>
