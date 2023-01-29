5ce7e368801cde92928be0841f2ea79c8c2330de
marcinlawnik <marcin@lawniczak.me>
2023-01-29 15:28:20 +0100

    Add private coma replacer

--- server/resources/views/liczymy/box/count.blade.php
+++ server/resources/views/liczymy/box/count.blade.php
@@ -382,7 +382,6 @@
             $("#total").text(sum.toFixed(2));
 
         }
-        @if(Auth::user()->hasAnyRole(['admin', 'superadmin']))
         document.getElementById('amount_EUR').addEventListener('change', replaceComa);
         document.getElementById('amount_GBP').addEventListener('change', replaceComa);
         document.getElementById('amount_USD').addEventListener('change', replaceComa);
@@ -390,10 +389,8 @@
         {{-- Enterom zapobiega wymagany checkbox --}}
         function replaceComa(input) {
             const input_value = input.target.value;
-            input.target.value = input_value.replace('.', ',');
+            input.target.value = input_value.replace(',', '.');
         }
-        @endif
-
     </script>
 @endpush
 
