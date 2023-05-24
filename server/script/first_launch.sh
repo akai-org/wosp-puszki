101b30b456cdde4a6f4b2f81e699ee114508ffec
Damian S <damian.sroczynski.22@gmail.com>
2023-05-24 20:20:11 +0200

    Revert "Generowanie docsów API przy setupie dockera"
    This reverts commit 9b3a7157f663ffe65d39975d0c34083718c97d5e.

--- server/script/first_launch.sh
+++ server/script/first_launch.sh
@@ -5,4 +5,3 @@ cp .env.example .env
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan migrate:fresh --seed
 docker-compose exec web chown -R www-data:www-data /app/storage/
-docker-compose exec app php artisan l5-swagger:generate
