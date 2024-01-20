402d9571c8de70dd1ad7939b8d4164ea4d0dee3a
Patryk Marczak <patryk.marczak@defuseddata.com>
2024-01-20 17:48:08 +0100

    Modal always centered and no scroll

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -4,36 +4,51 @@ import Title from 'antd/lib/typography/Title';
 import { Icon } from '@iconify/react';
 import { useNavigate } from 'react-router-dom';
 import { Header } from 'antd/lib/layout/layout';
+import { useEffect } from 'react';
+import { useSidebarStateContext } from '@/utils';
 
-export const Modal = ({ children, title, backRoute }: { children: JSX.Element, title: string, backRoute?: string }) => {
+export const Modal = ({
+  children,
+  title,
+  backRoute,
+}: {
+  children: JSX.Element;
+  title: string;
+  backRoute?: string;
+}) => {
+  const navigate = useNavigate();
+  const { show } = useSidebarStateContext();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden';
+    return () => {
+      document.body.style.overflowY = 'scroll';
+    };
+  }, []);
 
-    return (
-        <Layout className={s.wrapper}>
-            <Space direction='vertical' className={s.container}>
-                <Header className={s.header}>
-                    <Title level={4} className={s.title}>
-                        {title}
-                    </Title>
-                    <Button
-                        className={s.backButton}
-                        type="primary"
-                        onClick={() => backRoute ? navigate(backRoute) : navigate(-1)}
-                    >
-                        <Icon
-                            className={s.logOutButtonInside_icon}
-                            icon="ic:round-log-out"
-                            color="#FFFFFF"
-                            width="20"
-                            height="20"
-                        />
-                    </Button>
-                </Header>
-                <Space className={s.content}>
-                    {children}
-                </Space>
-            </Space>
-        </Layout>
-    )
-}
+  return (
+    <Layout className={[s.wrapper, show ? s.sidebarShown : s.sidebarHidden].join(' ')}>
+      <Space direction="vertical" className={s.container}>
+        <Header className={s.header}>
+          <Title level={4} className={s.title}>
+            {title}
+          </Title>
+          <Button
+            className={s.backButton}
+            type="primary"
+            onClick={() => (backRoute ? navigate(backRoute) : navigate(-1))}
+          >
+            <Icon
+              className={s.logOutButtonInside_icon}
+              icon="ic:round-log-out"
+              color="#FFFFFF"
+              width="20"
+              height="20"
+            />
+          </Button>
+        </Header>
+        <Space className={s.content}>{children}</Space>
+      </Space>
+    </Layout>
+  );
+};
