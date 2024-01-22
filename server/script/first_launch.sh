420c8d367cacdfa76287a02b290101e3d84ce3dc
kabix09 <kabix.009@gmail.com>
2024-01-22 21:00:29 +0100

    Fix first_launch script

--- server/script/first_launch.sh
+++ server/script/first_launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp .env.example .env
+docker compose exec app chmod 777 -R storage
+
+docker compose exec app cp .env.example .env
 docker compose exec app php artisan key:generate
 docker compose exec app php artisan migrate:fresh --seed
 docker compose exec app php artisan l5-swagger:generate
