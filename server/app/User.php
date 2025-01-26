dc4c2ef7f31fb289a329ce61388a58cb44744b47
marcinlawnik <marcin@lawniczak.me>
2025-01-26 11:34:30 +0100

    Fix missing user id

--- server/app/User.php
+++ server/app/User.php
@@ -32,6 +32,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+    }
+
     public function roles()
     {
         return $this->belongsToMany(Role::class);
