90b7fd9509ccfd3524637f316e28ac8c183edc56
Cherit <f.szostak44@gmail.com>
2026-01-24 12:36:17 +0100

    Fix login redirect path

--- server/app/Http/Controllers/Auth/LoginController.php
+++ server/app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'main';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
